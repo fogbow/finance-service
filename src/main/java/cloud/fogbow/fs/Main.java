f207e7e267faf95de658c8363d69bdb56d70c9ff
Armstrong Goes <armstrongmsg@gmail.com>
2021-02-05 12:37:17 -0300

    Initial project structure

---
+++ src/main/java/cloud/fogbow/fs/Main.java
@@ -0,0 +1,13 @@
+package cloud.fogbow.fs;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class Main implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+	}
+
+}
