df459aae2f306d7c54368c4ec12dbb83795f8e64
Armstrong Goes <armstrongmsg@gmail.com>
2021-02-05 17:49:30 -0300

    Added support to FinanceAuthorizationPlugin

--- src/main/java/cloud/fogbow/fs/api/parameters/AuthorizableUser.java
+++ src/main/java/cloud/fogbow/fs/api/parameters/AuthorizableUser.java
@@ -1,16 +1,38 @@
 package cloud.fogbow.fs.api.parameters;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
 
 public class AuthorizableUser {
+	private static final String USER_ID_KEY = "userId";
+	private static final String OPERATION_PARAMETERS_KEY = "operationParameters";
 	private String userId;
-	private HashMap<String, String> operationParameters;
+	private String operationParameters;
 	
+	public AuthorizableUser() {
+		
+	}
+	
+	public AuthorizableUser(String userId, Map<String, String> operationParameters) {
+		Gson gson = new Gson();
+		this.userId = userId;
+		this.operationParameters = gson.toJson(operationParameters);
+	}
+
 	public String getUserId() {
 		return userId;
 	}
 	
 	public HashMap<String, String> getOperationParameters() {
-		return operationParameters;
+		return new Gson().fromJson(operationParameters, HashMap.class);
 	}
+	
+    public Map<String, String> asRequestBody() {
+        HashMap<String, String> body = new HashMap<String, String>();
+        body.put(USER_ID_KEY, this.userId);
+        body.put(OPERATION_PARAMETERS_KEY, this.operationParameters);
+        return body;
+    }
 }
