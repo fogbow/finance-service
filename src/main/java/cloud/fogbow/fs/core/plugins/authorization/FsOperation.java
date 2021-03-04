3157c044304277ac40fce53990b73a0280d63769
Armstrong Goes <armstrongmsg@gmail.com>
2021-03-04 18:49:22 -0300

    Added explanation to FsOperation trivial equals implementation

--- src/main/java/cloud/fogbow/fs/core/plugins/authorization/FsOperation.java
+++ src/main/java/cloud/fogbow/fs/core/plugins/authorization/FsOperation.java
@@ -3,7 +3,11 @@ package cloud.fogbow.fs.core.plugins.authorization;
 import cloud.fogbow.common.models.FogbowOperation;
 
 public class FsOperation extends FogbowOperation {
-	// TODO explain this trivial equals implementation
+	// The purpose of this implementation is to 
+	// override the default behavior of equals
+	// (check if the objects are exactly the same),
+	// allowing some tests using mocked methods 
+	// which receive FsOperation objects as argument.
 	@Override
 	public boolean equals(Object o) {
 		return true;
