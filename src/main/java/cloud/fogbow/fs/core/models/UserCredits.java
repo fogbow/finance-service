1e21cff33a0b33d5f5aab97c1675d3d7735a09b1
Armstrong Goes <armstrongmsg@gmail.com>
2021-04-12 17:45:17 -0300

    Added tests to UserCredits

--- src/main/java/cloud/fogbow/fs/core/models/UserCredits.java
+++ src/main/java/cloud/fogbow/fs/core/models/UserCredits.java
@@ -1,5 +1,7 @@
 package cloud.fogbow.fs.core.models;
 
+import cloud.fogbow.common.exceptions.InvalidParameterException;
+import cloud.fogbow.fs.constants.Messages;
 import cloud.fogbow.fs.core.plugins.payment.ResourceItem;
 
 public class UserCredits {
@@ -14,11 +16,26 @@ public class UserCredits {
         this.credits = 0.0;
     }
 
-    public void deduct(ResourceItem resourceItem, Double valueToPayPerTimeUnit, Double timeUsed) {
+    public void deduct(ResourceItem resourceItem, Double valueToPayPerTimeUnit, Double timeUsed) 
+            throws InvalidParameterException {
+        if (valueToPayPerTimeUnit < 0) {
+            throw new InvalidParameterException(String.format(Messages.Exception.INVALID_VALUE_TO_PAY, 
+                    resourceItem.toString(), valueToPayPerTimeUnit));
+        }
+        
+        if (timeUsed < 0) {
+            throw new InvalidParameterException(String.format(Messages.Exception.INVALID_TIME_USED, 
+                    resourceItem.toString(), valueToPayPerTimeUnit));
+        }
+        
 	    this.credits -= valueToPayPerTimeUnit*timeUsed; 
 	}
 
-    public void addCredits(Double creditsToAdd) {
+    public void addCredits(Double creditsToAdd) throws InvalidParameterException {
+        if (creditsToAdd < 0) {
+            throw new InvalidParameterException(Messages.Exception.CANNOT_ADD_NEGATIVE_CREDITS);
+        }
+        
         this.credits += creditsToAdd;
     }
     
